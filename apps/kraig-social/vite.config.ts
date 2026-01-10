e780b1b9d60dc8d5595e751ec9acc62d322377d3
Kraig Walker <1294877+KraigWalker@users.noreply.github.com>
2026-01-10 17:17:28 +0000

    refactor: simplify addCrossoriginAnonymous plugin structure in Vite config

--- apps/kraig-social/vite.config.ts
+++ apps/kraig-social/vite.config.ts
@@ -1,24 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 const addCrossoriginAnonymous = () => ({
-  name: 'add-crossorigin-anonymous',
-  transformIndexHtml: {
-    enforce: 'post',
-    transform(html: string) {
-      const withScripts = html.replace(
-        /<script\b(?![^>]*\bcrossorigin\b)/g,
-        '<script crossorigin="anonymous"',
-      )
-      return withScripts.replace(
-        /<link\b(?![^>]*\bcrossorigin\b)(?=[^>]*\brel=(?:"stylesheet"|'stylesheet'|stylesheet))/g,
-        '<link crossorigin="anonymous"',
-      )
-    },
+  name: "add-crossorigin-anonymous",
+  transformIndexHtml(html: string) {
+    const withScripts = html.replace(
+      /<script\b(?![^>]*\bcrossorigin\b)/g,
+      '<script crossorigin="anonymous"',
+    );
+    return withScripts.replace(
+      /<link\b(?![^>]*\bcrossorigin\b)(?=[^>]*\brel=(?:"stylesheet"|'stylesheet'|stylesheet))/g,
+      '<link crossorigin="anonymous"',
+    );
   },
-})
+});
 
 export default defineConfig({
   plugins: [react(), addCrossoriginAnonymous()],
-})
+});
