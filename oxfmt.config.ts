a55b90d9e9334f96e03e8543c4f529b451ea7bde
Kraig Walker <1294877+KraigWalker@users.noreply.github.com>
2026-06-08 17:54:07 +0100

    add oxc-autoinstaller and update configuration files for oxfmt and oxlint

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,5 +1,14 @@
-import type { OxfmtConfig } from "oxfmt";
+import type { OxfmtConfig } from 'oxfmt';
 
 export default {
-  printWidth: 80,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  insertFinalNewline: true,
+  embeddedLanguageFormatting: 'auto',
+  ignorePatterns: ['dist/**', '*.min.js'],
+  sortPackageJson: true,
 } satisfies OxfmtConfig;
